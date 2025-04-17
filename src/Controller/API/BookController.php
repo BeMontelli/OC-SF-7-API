6c0fab3e9711227a1c9a2a6bffed9b12f76c7a66
Benjamin.M <benjmonte@outlook.fr>
2025-04-17 12:08:53 +0200

    Added Serializer + index api resource

--- src/Controller/API/BookController.php
+++ src/Controller/API/BookController.php
@@ -6,16 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\BookRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 final class BookController extends AbstractController
 {
     #[Route('/api/v1/books', name: 'app_api_book', methods: ['GET'])]
-    public function index(BookRepository $bookRepository): JsonResponse
+    public function index(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
         $bookList = $bookRepository->findAll();
+        $jsonBookList = $serializer->serialize($bookList, 'json');
 
-        return new JsonResponse([
-            'books' => $bookList,
-        ]);
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [],true);
     }
 }
